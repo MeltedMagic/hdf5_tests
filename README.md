a987c76e12aee94b82feba14ac1d6992406ae8ff
manu <manu.samala7@gmail.com>
2022-08-20 19:11:07 -0400

    test for first dataset

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # hdf5_tests
-Read mat7.3 files with h5py, mat files are from here: https://github.com/skjerns/mat7.3
+Read mat7.3 files with h5py, mat files used are available here under tests: https://github.com/skjerns/mat7.3
+This is my attempt to open all the data in h5py as best as I can.
+
+# To Do
+* Look into dtypes, type handling
+* Look into module mode and script mode
+
+# Bibliography
+## HDF5
+* https://github.com/skjerns/mat7.3
+* https://stackoverflow.com/questions/28541847/how-convert-this-type-of-data-hdf5-object-reference-to-something-more-readable
+* https://docs.h5py.org/en/stable/index.html
+* https://stackoverflow.com/questions/63136526/h5py-issues-to-correctly-read-a-table-class-stored-in-matlab-mat-7-3
+* http://davis.lbl.gov/Manuals/HDF5-1.8.7/UG/11_Datatypes.html
+* http://davis.lbl.gov/Manuals/HDF5-1.8.7/index.html
+* https://pythonnumericalmethods.berkeley.edu/notebooks/chapter11.05-HDF5-Files.html
+* https://www.christopherlovell.co.uk/blog/2016/04/27/h5py-intro.html
+* https://stackoverflow.com/questions/31037088/discovering-keys-using-h5py-in-python3
+* https://stackoverflow.com/questions/29852481/reading-all-variables-in-a-mat-file-with-python-h5py/29856030
+* https://stackoverflow.com/questions/46733052/read-hdf5-file-into-numpy-array
+
+
+## NetCDF
+* https://docs.unidata.ucar.edu/nug/current/netcdf_data_set_components.html#object_name -- specifies variable name requirments
+* https://docs.unidata.ucar.edu/nug/current/index.html
+* https://cfconventions.org/Data/cf-conventions/cf-conventions-1.7/build/ch02s03.html
+* https://towardsdatascience.com/basic-data-structures-of-xarray-80bab8094efa
+
+
+# Auxillary
+* https://stackoverflow.com/questions/51721862/detecting-mat-version-using-python
+* https://cfconventions.org/cf-conventions/cf-conventions.pdf
+* https://realpython.com/python-type-checking/
+* https://jakevdp.github.io/PythonDataScienceHandbook/02.01-understanding-data-types.html
+* https://training.digitalearthafrica.org/en/latest/index.html
+* https://www.atmos.albany.edu/facstaff/ktyle/atm533/landing-page.html
---
+++ data.py
@@ -0,0 +1,147 @@
+# Objective: Read mat7.3 datasets using h5py
+
+# Import packages
+import h5py
+import numpy as np
+
+file = h5py.File('testfile1.mat', 'r')
+
+# print(list(file)) # List file structure
+
+# 'file/data'
+    # print(list(file['data'])) # List structure of 'data'
+
+## Print Numbers
+int8 = np.array(file['data/int8_'])
+int8 = int8.flatten()                   # Remove array nesting, extract data
+    # print(int8[0])
+
+uint16 = np.array(file['data/uint16_'])
+uint16 = uint16.flatten()
+    # print(uint16[0])
+
+single = np.array(file['data/single_'])
+single = single.flatten()
+    # print(single[0])
+
+double = np.array(file['data/double_'])
+double = double.flatten()
+    # print(double[0])
+
+## Print Char
+char = np.array(file['data/char_'])
+char = (char.flatten())[0]
+    # print(chr(char))
+
+## Print Bool
+boole = np.array(file['data/bool_'])
+boole = boole.flatten()
+    # print(boole[0])                         # print(bool(boole)) -- convert to bool
+
+## Print Float Array
+floatArr = np.array(file['data/arr_float'])
+    # print(list(floatArr))
+
+## Print Missing --> None
+missing = file.get('data/missing_/Full')
+    # print(missing)
+missing = file.get('data/missing_/WL')
+    # print(missing)
+
+## Print Complex Number
+comPlex = file.get('data/complex_')
+    # print(comPlex.dtype)                    # Get numpy data type
+comPlex = (np.array(comPlex)).flatten()
+    # print(complex(comPlex[0][0], comPlex[0][1]))
+
+## Print Complex Array
+comArr = (np.array(file.get('data/complex3_'))).flatten()
+    # print(comArr[0][0] + 1j*comArr[0][1])
+
+## Print Cell Char
+num = 3
+cellChar = file['data/cell_char_']
+    # print(cellChar)
+cellChar = (np.array(cellChar)).flatten()
+    # print(cellChar[num])                          # This prints out the reference, we need to get
+st = cellChar[num]                            # The HDF5 object
+str1 = (np.array(file[st])).flatten()       # Get the object, Un-nest the array, (*0)
+str1 = ''.join(chr(i) for i in str1)
+    # print(str1)
+
+## Print 'string'
+str2 = file['data/string_']
+str2 = (np.array(str2)).flatten()
+str2 = ''.join(chr(i) for i in str2)
+    # print(str2)
+
+## Print 'struct_'
+struct = file.get('data/struct_')
+    # print(list(struct))                             # Get structure of matlab struct
+    # print((np.array(struct['test'])).flatten())     # Get contents of struct
+
+## Print 'struct2_'
+struct_2 = file.get('data/struct2_')
+print(list(struct_2))
+
+### Print 'struct2_/type'
+num = 1
+tYpe = struct_2['type']
+    # print(tYpe[num][0])                                     # The second index un-nests the object
+big = (np.array(file[tYpe[num][0]])).flatten()           # out of the array
+big = ''.join(chr(i) for i in big)
+    # print(big)
+
+### Print 'struct2_/color'
+    # print(list(struct_2['color']))
+color = struct_2['color']
+color0 = struct_2['color'][1][0]
+    # print(color0)
+    # print(file[color0])
+color0 = (np.array(file[color0]).flatten())
+str3 = ''.join(chr(i) for i in color0)
+    # print(str3)
+
+### Print 'struct2_/x'
+print(list(file['data/struct2_/x']))
+print(len( list(file['data/struct2_/x']) )  )
+arr = file['data/struct2_/x']
+xArr = file[arr[1][0]]
+print(np.transpose(xArr))                               # Matrix now follows the shape shown in matlab
+    # print(np.array(file[struct_2['x'][0][0]]))
+    # print(np.array(file[struct_2['x'][1][0]]))
+
+
+# 'file/keys'
+
+## Convert int array to char array to string
+#print(file['keys'])                    # Char array
+str0 = np.array(file['keys'])           # Turn hdf5 char array to numpy array
+str0 = str0.flatten()                   # turn array to 1 row instead of an array of array of an int
+str0 = ''.join(chr(i) for i in str0)    # turn num array to char array
+# print(f"key char array: {str0}")
+
+
+
+# 'file/secondvar'
+
+
+
+
+# Comments:
+# •••••••••••••••••••••••••••••••••••••••••••••••••••
+# •••••••••••••••••••••••••••••••••••••••••••••••••••
+# •••••••••••••••••••••••••••••••••••••••••••••••••••
+# print(file.keys()) # List keys
+# print(list(file['data/arr_bool/'][0]))
+# print(file['data/arr_bool/']) # Access groups in datasets
+                                # Get data from structures in datasets
+
+
+# (*0)
+"""
+Nested Array: [ [0], [1], [2], [3] ]
+Un-nested Array: [0, 1, 2, 3]           -- This makes it to where
+                                           we loop over one dimension
+                                           instead of 2
+"""
